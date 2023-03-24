4be24c2bb67e48f6e2f9d3314d4cc71a23908f4f
RamaRamdani03 <ramaramdani449@gamil.com>
2023-03-24 15:50:38 +0700

    memasukkan while

--- belajar_insertionsort/belajar_insertionsort.cpp
+++ belajar_insertionsort/belajar_insertionsort.cpp
@@ -5,7 +5,23 @@ int arr[20];
 int n;
 void input() {
 	while (true) {
-		
+		cout << "masukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	
 
 
 
